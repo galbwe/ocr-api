d1208941308bc221aefdb3562f809647f737ea42
Wes Galbraith <galbwe92@gmail.com>
2019-12-15 20:54:35 -0700

    added dao for image

--- src/ocr_api/dao/redis_image_dao.py
+++ src/ocr_api/dao/redis_image_dao.py
@@ -1,3 +1,42 @@
+from models.image import Image
 
-class ImageDao:
-    
+
+class RedisImageDao:
+    def __init__(self, redis_client, image_schema, uuid_generator, collection='images'):
+        self.redis_client = redis_client
+        self.image_schema = image_schema
+        self.uuid_generator = uuid_generator
+        self.collection = collection
+
+    def get_image_by_id(self, id):
+        if self.redis_client.collection_has_key(self.collection, id):
+            image_data = self.redis_client.get(id)
+            return self.image_schema.load(image_data)
+        raise ValueError('Collection {self.collection} has no key {id}.')
+
+    def update_image(self, id, image):
+        if self.redis_client.collection_has_key(self.collection, id):
+            image.id = id
+            image_data = self.image_schema.dump(image)
+            self.redis_client.set(id, image_data)
+            return image
+        raise ValueError(f'Collection {self.collection} has no key {id}.')
+
+    def delete_image(self, id):
+        if self.redis_client.collection_has_key(self.collection, key):
+            image_data = self.redis_client.get(key)
+            self.redis_client.delete(key)
+            return self.image_schema.load(image_data)
+        raise ValueError(f'Collection {self.collection} has no key {id}.')
+
+    def insert_image(self, image):
+        id = next(self.uuid_generator)
+        while not self.redis_client.collection_has_key(self.collection, id):
+            id = next(self.uuid_generator)
+        image.id = id
+        image_data = self.image_schema.dump(image)
+        self.redis_client.set(key, image_data)
+        return image
+
+    def get_all_image_ids(self):
+        return self.redis_client.hkeys(self.collection)
--- src/ocr_api/redis_client.py
+++ src/ocr_api/redis_client.py
@@ -16,9 +16,34 @@ class RedisClient:
 
     def set(self, key, value):
         string_value = json.dumps(value)
-        self._r.set(key, string_value)
+        self._r.set(str(key), string_value)
 
     def get(self, key):
         binary_data = self._r.get(key)
         string_data = binary_data.decode()
         return json.loads(string_data)
+
+    def hset(self, collection, key, value):
+        string_value = json.dumps(value)
+        self._r.hset(collection, str(key), string_value)
+
+    def hget(self, collection, key):
+        binary_data = self._r.hget(collection, key)
+        string_data = binary_data.decode()
+        return json.loads(string_data)
+
+    def hexists(self, collection, key):
+        return self._r.hexists(collection, key)
+
+    def delete(self, key):
+        self._r.delete(key)
+
+    def key_exists(self, key):
+        return self._r.exists(key) > 0
+
+    def hkeys(self, hash):
+        binary_keys = self._r.hkeys(hash)
+        return [key.decode() for key in binary_keys]
+
+    def collection_has_key(collection, key):
+        return key in self.hkeys(collection)
